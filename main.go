927c8541e4e2a20159101de6adc26ec425c24e09
wylzabc <wylzabc@gmail.com>
2019-06-22 17:03:57 +0800

    add ping/pong

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"time"
 )
+
+var router *gin.Engine
+
+func InitRouter() {
+	//  gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
+	router = gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+}
 
 func main() {
 	var numbers []int
@@ -13,4 +27,8 @@ func main() {
 		fmt.Printf("len: %d  cap: %d  pointer: %p\n", len(numbers), cap(numbers), numbers)
 	}
 	fmt.Println(time.Now())
+
+	InitRouter()
+	router.Run() // listen and serve on 0.0.0.0:8080
+
 }
