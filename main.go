d85da12e1c2b0ec5c7f039697edbd3b019266e5c
wylzabc <wylzabc@gmail.com>
2019-06-22 16:12:05 +0800

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers []int
+
+	for i := 0; i < 10; i++ {
+		numbers = append(numbers, i)
+		fmt.Printf("len: %d  cap: %d  pointer: %p\n", len(numbers), cap(numbers), numbers)
+	}
+}
